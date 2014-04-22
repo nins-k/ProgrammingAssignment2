1fa920817472d2e77d56a936ce3f16317bff71f4
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Inverse caching is functional

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(new_inverse) inverse <<- new_inverse
+        getInverse <- function() inverse
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        new_inverse <- x$getInverse()
+        if(!is.null(new_inverse)) {
+                message("Using cached data")
+                return(new_inverse)
+        }
+        data <- x$get()
+        new_inverse <- solve(data)
+        x$setInverse(new_inverse)
+        new_inverse
 }
+
